6b6a0722459e0ef42823330ae327c734b4b0414b
Alexotieno1717 <alexotieno900@gmail.com>
2022-07-21 12:45:01 +0300

    Authentication on Frontend

--- plugins/apiFetch.js
+++ plugins/apiFetch.js
@@ -1,6 +1,32 @@
+import Cookies from 'js-cookie'
 
 export default defineNuxtPlugin(nuxtApp => {
-    const config = useRuntimeConfig()
+  const config = useRuntimeConfig()
+
+  nuxtApp.provide(
+    'apiFetch',
+    $fetch.create({
+      baseURL: config.BASE_URL,
+      credentials: 'include',
+      headers: {
+        Accept: 'application/json',
+        'X-XSRF-TOKEN': Cookies.get('XSRF-TOKEN'),
+      },
+    })
+  )
+})
+
+// import Cookies from 'js-cookie'
+
+// export default defineNuxtPlugin(nuxtApp => {
+//     const config = useRuntimeConfig()
     
-    nuxtApp.provide('apiFetch', $fetch.create({ baseURL: config.BASE_URL }))
-  })
+//     nuxtApp.provide('apiFetch', $fetch.create({ 
+//       baseURL: config.BASE_URL,
+//       credentials: 'include',
+//       Headers: {
+//         Accept: 'application/json',
+//         'X-XSRF-TOKEN': Cookies.get('XSRF-TOKEN'),
+//       },
+//     }))
+//   })
